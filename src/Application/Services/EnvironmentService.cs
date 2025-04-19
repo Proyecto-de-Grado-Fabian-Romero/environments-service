b26067509a7e6118ffb5b2320c0e6e26a74cb49f
just-fabian <fabian130404@gmail.com>
2025-04-19 19:31:21 -0400

    SPACIO-43 feat: delete get pre signed url

--- src/Application/Services/EnvironmentService.cs
+++ src/Application/Services/EnvironmentService.cs
@@ -1,9 +1,7 @@
-using System.Text.Json;
 using AutoMapper;
 using EnvironmentsService.Src.Application.DTOs.Get;
 using EnvironmentsService.Src.Application.DTOs.GetRequest;
 using EnvironmentsService.Src.Application.Interfaces;
-using EnvironmentsService.Src.Domain.Entities;
 using EnvironmentsService.src.Domain.Interfaces;
 
 namespace EnvironmentsService.Src.Application.Services;
@@ -12,8 +10,6 @@ public class EnvironmentService(IEnvironmentRepository repository, IMapper mappe
 {
     private readonly IEnvironmentRepository _repository = repository;
     private readonly IMapper _mapper = mapper;
-
-    private readonly HttpClient _httpClient = new ();
 
     public async Task<PagedResult<GetAllEnvironmentDto>> GetAvailableEnvironmentsAsync(GetAvailableEnvironmentsRequest request, int page, int limit)
     {
@@ -38,54 +34,6 @@ public class EnvironmentService(IEnvironmentRepository repository, IMapper mappe
         var environment = await _repository.GetSingleEnvironment(publicId);
         var environmentDto = _mapper.Map<EnvironmentDto>(environment);
 
-        if (environment != null)
-        {
-            await Parallel.ForEachAsync(
-            Enumerable.Range(0, 5),
-            new ParallelOptions { MaxDegreeOfParallelism = 10 },
-            async (i, _) =>
-            {
-                var photoUrls = new List<string>();
-
-                foreach (var photo in environment.Photos.OrderBy(p => p.Order).Take(4))
-                {
-                    var url = await GeneratePhotoUrlAsync(photo);
-                    photoUrls.Add(url);
-                }
-
-                environmentDto.PhotoUrls = photoUrls;
-            });
-        }
-
         return environmentDto;
-    }
-
-    private async Task<string> GeneratePhotoUrlAsync(EnvironmentPhoto photo)
-    {
-        var requestUrl = $"http://localhost:5116/api/image/url?fileId={photo.FileId}&fileName={photo.FileName}";
-
-        var response = await _httpClient.GetAsync(requestUrl);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            return string.Empty;
-        }
-
-        var content = await response.Content.ReadAsStringAsync();
-
-        try
-        {
-            var json = JsonDocument.Parse(content);
-            var resultUrl = json.RootElement
-                .GetProperty("url")
-                .GetProperty("result")
-                .GetString();
-
-            return resultUrl ?? string.Empty;
-        }
-        catch (Exception)
-        {
-            return string.Empty;
-        }
     }
 }
